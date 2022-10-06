db00733a866e23d64d1b04f0919619a8ed709a46
Ben Holgate <holgateb@hotmail.com>
2022-10-05 18:01:07 -0700

    wrote select statements and join statments.

--- db/index.js
+++ db/index.js
@@ -6,8 +6,43 @@ class Query {
     }
 
     findAllDepartments(){
+        // WHEN I choose to view all departments
+        // THEN I am presented with a formatted table showing department names and department ids
         return this.db.promise().query('SELECT * FROM department;')
     }
+
+    findAllRoles(){
+     // WHEN I choose to view all roles
+    // THEN I am presented with the role title, role id, the department that role belongs to, and the salary for that role
+        return this.db.promise().query('SELECT role.title, role.id, department.name, role.salary from role JOIN department ON role.department_id = department.id')
+    }
+
+    findAllEmployees(){
+    // WHEN I choose to view all employees
+    // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+        return this.db.promise().query('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT (manager.first_name, " ", manager.last_name) AS fullname from employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id JOIN employee AS manager ON manager.id = employee.manager_id')
+    }
+
+    insertDepartment(){
+        return this.db.promise().query('INSERT')
+    }
+    
+    insertRole(){
+        return this.db.promise().query('')
+    }
+
+    
+    insertEmployee(){
+        return this.db.promise().query('')
+    }
+
+    
+    updateEmployeeRole(){
+        return this.db.promise().query('')
+    }
+
+   
+
 
 }
 
--- server.js
+++ server.js
@@ -1,71 +1,140 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const db = require("./db");
+require("console.table");
 
 //View all departments
-//SELECT * FROM department
+// //SELECT * FROM department
 
-//View all roles
-//SELECT * FROM role
+// //View all roles
+// //SELECT * FROM role
 
-//View all employees
-//SELECT * FROM employee
+// //View all employees
+// //SELECT * FROM employee
 
-//Create new departments
+// //Create new departments
 
-//Prompt the user for the "name" of the department
+// //Prompt the user for the "name" of the department
 
-    //THEN run the query
-    //INSERT INTO department (name)
-    //VALUES ("Sales")
+//     //THEN run the query
+//     //INSERT INTO department (name)
+//     //VALUES ("Sales")
 
-        //THEN ask the user what they want to do next
+//         //THEN ask the user what they want to do next
 
-//Create a new role
-function createRole () {
+// //Create a new role
+// function createRole () {
 
-//GET the existing departments from the 'deparment' table
+// //GET the existing departments from the 'deparment' table
 
-    //THE prompt the user for the "title", "salary",  and "department" for the role
+//     //THE prompt the user for the "title", "salary",  and "department" for the role
 
-        //THEN run the query
-        //INSERT INTO role (title, salary, department_id)
-        //VALUES ("Manager", "120000, 1")
+//         //THEN run the query
+//         //INSERT INTO role (title, salary, department_id)
+//         //VALUES ("Manager", "120000, 1")
 
-            //THEN ask the user what they want to do next
-        }
+//             //THEN ask the user what they want to do next
+//         }
 
-const createPost = async () => {
-    const users = await db.query("SELECT * FROM users");
+// const createPost = async () => {
+//     const users = await db.query("SELECT * FROM users");
 
-    const userChoices = users.map( user => {
-        name: user.username,
-        value: user.id
-    }) };
+//     const userChoices = users.map( user => {
+//         name: user.username,
+//         value: user.id
+//     }) };
 
-    const answers = inquirer.prompt ([
-        {
-            message: "What is the title of the post?",
-            name: "title",
-            type: "input"
-        },
-        {
-            message: "What is the content of the post?",
-            name: "content",
-            type: "input"
-        },
-        {
-            message: "What is the author of the post?",
-            name: "title",
-            type: "list",
-            choices: userChoices
-        }
-    ]);
+//     const answers = inquirer.prompt ([
+//         {
+//             message: "What is the title of the post?",
+//             name: "title",
+//             type: "input"
+//         },
+//         {
+//             message: "What is the content of the post?",
+//             name: "content",
+//             type: "input"
+//         },
+//         {
+//             message: "What is the author of the post?",
+//             name: "title",
+//             type: "list",
+//             choices: userChoices
+//         }
+//     ]);
 
-createPost();
-
+// createPost();
 
 // GIVEN a command-line application that accepts user input
 // WHEN I start the application
 // THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+function start() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "q1",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Quit",
+      ],
+    })
+    .then((answers) => {
+      console.log(answers.q1);
+      switch (answers.q1) {
+        case "View all departments":
+          viewAllDepartments();
+          break;
+        case "View all roles":
+          viewAllRoles();
+          break;
+        case "View all employees":
+          viewAllEmployees();
+          break;
+        case "Add a department":
+          break;
+        case "Add a role":
+          break;
+        case "Add an employee":
+          break;
+        case "Update an employee role":
+          break;
+        default:
+          process.exit();
+          break;
+      }
+    });
+}
+
+function viewAllDepartments() {
+  db.findAllDepartments()
+    .then(([data]) => {
+      console.table(data);
+    })
+    .then(() => start());
+}
+
+function viewAllRoles() {
+  db.findAllRoles()
+    .then(([data]) => {
+      console.table(data);
+    })
+    .then(() => start());
+}
+
+function viewAllEmployees() {
+  db.findAllEmployees()
+    .then(([data]) => {
+      console.table(data);
+    })
+    .then(() => start());
+}
+
 // WHEN I choose to view all departments
 // THEN I am presented with a formatted table showing department names and department ids
 // WHEN I choose to view all roles
@@ -79,4 +148,6 @@ createPost();
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 // WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+
+start();
